009d5adc5242b807ce6734d95b4103c0c7e490eb
sharky <zeebing@gmail.com>
2019-05-06 00:30:38 -0400

    added 3 mysql queries

--- config/orms.js
+++ config/orms.js
@@ -1,14 +1,26 @@
 const connection = require("../.config/connection.js");
 
 var orm = {
-    selectAll: function(){
-        var queryString = "SELECT * FROM yums"
+    selectAll: function(table){
+        var queryString = "SELECT * FROM ?";
+        connection.query(queryString, [table], function(err, result){
+            if(err) throw err;
+            console.log(result);
+        })
     },
-    insertOne: function(){
-        var queryString = "INSERT INTO yums"
+    insertOne: function(table,name){
+        var queryString = "INSERT INTO ? (?) VALUES (?)";
+        connection.query(queryString,[table,"yum_name",name],function(err, result){
+            if(err) throw err;
+            console.log(result);
+        });
     },
-    updateOne: function(row){
-        var queryString = "";
+    updateOne: function(table,row,key,value){
+        var queryString = "UPDATE ? SET ? = ? WHERE id = ?";
+        connection.query(queryString,[table,key, value, row]), function(err, result){
+            if(err) throw err;
+            console.log(result);
+        };
     }
 };
 

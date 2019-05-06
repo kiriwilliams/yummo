42b7496243cd4deff3f1678f9c2de3e9d25b335c
sharky <zeebing@gmail.com>
2019-05-06 00:35:16 -0400

    started on routing with post

--- controllers/yums_controller.js
+++ controllers/yums_controller.js
@@ -4,3 +4,14 @@ const yum = require("../models/yum.js");
 
 
 
+router.get("/", function(req, res){
+
+});
+
+router.post("/api/yums", function(req, res){
+    yum.create(["name"],[req.body.name], function(result){
+        res.json({ id: result.insertId });
+    });
+});
+
+module.exports = router;
--- models/yum.js
+++ models/yum.js
@@ -1,2 +1,21 @@
 const orm = require("../config/orms.js");
 
+var yum = {
+    selectAll: function(callback){
+        orm.selectAll("yums", function(res){
+            callback(res);
+        });
+    },
+    insertOne: function(name, callback){
+        orm.insertOne("yums", name, function(res){
+            callback(res);
+        });
+    },
+    updateOne: function(row,key,value, callback){
+        orm.updateOne("yums",row,key,value, function(res){
+            callback(res);
+        });
+    }
+};
+
+module.exports = yum;

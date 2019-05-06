915861645314a2859f7800d2b97d9ba17c3991de
sharky <zeebing@gmail.com>
2019-05-06 00:35:57 -0400

    set up handlebars, imported routes, added listener

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/yums_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});

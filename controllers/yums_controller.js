014c1ca52320f01a84d07daa04729b427e38eed4
sharky <zeebing@gmail.com>
2019-05-07 13:41:39 -0400

    added res.redirect to post and put routes to refresh page

--- controllers/yums_controller.js
+++ controllers/yums_controller.js
@@ -19,13 +19,21 @@ router.post("/api/yums", function(req, res){
     console.log("post" + JSON.stringify(req.body));
     yum.insertOne([req.body.yum_name], function(result){
         res.json({ id: result.insertId });
+        
     });
+    res.redirect("/");
 });
 
 router.put("/api/yums", function(req,res){
     yum.updateOne([req.body.id], function(result){
-        console.log(result);
-    })
-})
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
+    });
+    res.redirect("/");
+});
 
 module.exports = router;

3c6ac9ff5b300e3ddd17b14b37bddf09dc51e7c7
sharky <zeebing@gmail.com>
2019-05-07 10:35:37 -0400

    trying to get linking between eat.js and main.handelbars

--- public/js/eat.js
+++ public/js/eat.js
@@ -1,6 +1,11 @@
 $(document).on("ready", function(){
+    console.log("hi eat.js");
+    
     $(".delete").on("click", function(){
         console.log("hi");
         $(this).parent.remove();
+    });
+    $("#submit").on("click", function(){
+        console.log("clicked susbmit");
     });
 });

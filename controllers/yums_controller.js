ce190ff8bc2b7f36cdcac1af6f44e25562a8801b
sharky <zeebing@gmail.com>
2019-05-08 11:28:50 -0400

    added route to serve handlebars index page

--- controllers/yums_controller.js
+++ controllers/yums_controller.js
@@ -5,29 +5,38 @@ var router = express();
 
 
 //GET route
-router.get("/api/yums", function (req, res) {
-    db.Yum.findAll({}).then(function (result) {
-        res.json(result);
+router.get("/", function (req, res) {
+    db.Yum.findAll({}).then(function (data) {
+        // console.log(data);
+        var hbrsobj = {
+            food: data
+        }
+        res.render("index", hbrsobj);
     });
 });
+
 
 // POST
 router.post("/api/yums", function (req, res) {
     db.Yum.create({
-        yum_name: req.body.name
+        yum_name: req.body.yum_name
+    }).then(function (result) {
+        res.json(result);
     });
 });
 
 // PUT
 router.put("/api/yums", function (req, res) {
+    console.log(req.body);
     db.Yum.update({
-        devoured: true,
-        where: {
-            id: req.body.id
-        }
-    }).then(function (result) {
-        res.json(result);
-    });
+        devoured: true
+    }, {
+            where: {
+                id: req.body.id
+            }
+        }).then(function (result) {
+            res.json(result);
+        });
 });
 
 module.exports = router;
